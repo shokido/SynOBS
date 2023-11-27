229189d3441d207ac8759a399eadb6694f0a6b14
skidode <skiod@jamstec.go.jp>
2023-11-27 14:01:05 +0900

    Change method of vertical integration

--- DIAGNOSTICS/SCRIPTS/calc_VINT.py
+++ DIAGNOSTICS/SCRIPTS/calc_VINT.py
@@ -4,7 +4,7 @@ import numpy as np
 # Python script for vertical integration
 varname_lev="lev"
 varnames_in=[]
-fnames_in=[];fnames_out=[];in_thres=[];varnames_out=[]
+fnames_in=[];fnames_out=[];in_thres=[];varnames_out=[];varunits_out=[]
 vint_1=[];vint_2=[]
 varnames_in.append("T");fnames_in.append("../TestFile/T_20200101_GLOBAL_360x180.nc")
 vint_1.append(0);vint_2.append(50);varnames_out.append("0-50mT")
@@ -20,7 +20,6 @@ varunits_out.append("PSU")
 fnames_out.append("0-300mS_20200101_GLOBAL_360x180.nc")
 
 
-dz=10
 for ifile in range(0,len(fnames_in)):
     ds_in=xr.open_dataset(fnames_in[ifile])
     da_var= xr.apply_ufunc(
@@ -28,7 +27,7 @@ for ifile in range(0,len(fnames_in)):
     ds_in[varname_lev], # depth
     ds_in[varnames_in[ifile]], # inocity
     input_core_dims=[['lev'], ['lev']],
-    kwargs={'x1': vint_1[ifile],'x2':vint_2[ifile], 'dx':dz},
+    kwargs={'x1': vint_1[ifile],'x2':vint_2[ifile]},
     vectorize=True,  # Vectorize function over remaining 'x' and 'y' dimensions
     output_dtypes=[float],)
     da_var=da_var/(vint_2[ifile]-vint_1[ifile])
