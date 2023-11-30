b30622ad6cbefc6e16ebd06485dd7db646955e59
skidode <skiod@jamstec.go.jp>
2023-12-01 00:09:11 +0900

    Modify S2S-AN name

--- SCRIPTS/S2S-AN/create_netCDFsample_S2SPL_Moor.py
+++ SCRIPTS/S2S-AN/create_netCDFsample_S2SPL_Moor.py
@@ -4,7 +4,7 @@ import datetime as dt
 import numpy as np
 import os
 
-dir_work="../../../S2S-AN"
+dir_work="../../S2S-AN"
 dt_start=dt.datetime(2003,1,1,0,0,0) # Start date of output
 dt_end=dt.datetime(2003,1,31,0,0,0)  # End date of output (for an initial test...terminate at 31/1/2003)
 #dt_end=dt.datetime(2022,12,31,0,0,0)  # End date of output
