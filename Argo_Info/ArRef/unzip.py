d364e111261d026998c399006fcadb4c2e6d2c3f
skidode <skiod@jamstec.go.jp>
2024-01-08 11:57:44 +0900

    Remove files

--- Argo_Info/ArRef/unzip.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-for iy in range(2003,2023):
-    command="gunzip argoasm"+str(iy)+".tar.gz"
-    subprocess.call(command.split())
-    command="tar -xvzf argoasm"+str(iy)+".tar"
-    subprocess.call(command.split())
